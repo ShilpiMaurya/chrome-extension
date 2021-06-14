888ab7ee3a0acd6f6450f312dd4ac47ddd2362f3
ShilpiMaurya <meshilpi94@gmail.com>
2021-06-14 12:30:33 +0530

    added: pop.js

--- popup.js
+++ popup.js
@@ -1,8 +1,12 @@
 const colorEl = document.getElementById("color-changer");
 
-colorEl.addEventListener("input", e => {
+colorEl.addEventListener("input", async e => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   const hexValue = e.target.value;
-  chrome.tabs.executeScript(null, {
-    code: `document.body.style.backgroundColor="${hexValue}"`
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: () => {
+      document.body.style.backgroundColor = hexValue;
+    }
   });
 });
